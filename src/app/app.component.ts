9550524836642df115ab003c126eff6215c2192f
admin1 <durganjaneyulu>
2021-04-06 11:19:47 +0530

    design dashbord

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,29 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'VendorDashbord';
+  flag:any
+  sidenavWidth = 4;
+  // ngStyle: string;
+  openFlag=0
+constructor(){
 }
+ngOnInit(){
+  this.flag ='Ar'
+
+}
+activeTab(value:any){
+  this.flag=value
+}
+
+  increase() {
+    console.log("this.openFlag",this.openFlag)
+    if(this.openFlag == 0){
+      this.openFlag=1
+      this.sidenavWidth = 18;
+      console.log("increase sidenav width");
+    }else{
+      this.openFlag = 0
+      this.sidenavWidth = 4;
+      console.log("decrease sidenav width");
+    }
+  }}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import "bootstrap";
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+import {MatDividerModule} from '@angular/material/divider';
+import {MatCardModule} from '@angular/material/card';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import { HttpClientModule } from '@angular/common/http';
+// import {MatIconModule} from '@angular/material/icon';
+import {MatListModule} from '@angular/material/list';
+import {MatMenuModule} from '@angular/material/menu';
+import { MatSliderModule } from '@angular/material/slider';
+import {MatSlideToggleModule} from '@angular/material/slide-toggle';
+// import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatTooltipModule } from '@angular/material/tooltip';
+
 
 @NgModule({
   declarations: [
@@ -10,7 +26,12 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatDividerModule,
+    MatCardModule,MatSidenavModule,MatSliderModule,MatMenuModule,MatListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
